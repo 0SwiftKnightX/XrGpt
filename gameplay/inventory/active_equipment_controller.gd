0153f181085a960a99bf03ba2e37447343c5ac77
0SwiftKnightX <113105682+0SwiftKnightX@users.noreply.github.com>
2026-09-20 00:47:26 -0400

    Add active equipment physical slot management

--- gameplay/inventory/active_equipment_controller.gd
+++ gameplay/inventory/active_equipment_controller.gd
@@ -8,12 +8,23 @@ extends Node3D
 var is_open := true
 var _board: Node3D
 var _camera: XRCamera3D
+var _slots: Array[XRGptItemSlot] = []
 
 func _ready() -> void:
 	_board = get_node_or_null("ActiveEquipmentBoard")
 	_camera = get_node_or_null(camera_path) as XRCamera3D
+	_cache_slots()
 	if _board:
 		_board.visible = is_open
+
+func _cache_slots() -> void:
+	_slots.clear()
+	if _board == null:
+		return
+	for child in _board.get_children():
+		if child is XRGptItemSlot:
+			_slots.append(child as XRGptItemSlot)
+	_slots.sort_custom(func(a, b): return a.slot_id < b.slot_id)
 
 func _process(_delta: float) -> void:
 	if not is_open or _board == null or _camera == null:
@@ -22,7 +33,23 @@ func _process(_delta: float) -> void:
 	_board.global_position = camera_transform.origin - camera_transform.basis.z * distance + Vector3.UP * vertical_offset
 	_board.look_at(camera_transform.origin, Vector3.UP)
 
+func get_slots() -> Array[XRGptItemSlot]:
+	return _slots
+
 func toggle() -> void:
 	is_open = not is_open
 	if _board:
 		_board.visible = is_open
+
+func place_item(instance: XRGptItemInstance) -> bool:
+	for slot in _slots:
+		if slot.set_item(instance):
+			return true
+	return false
+
+func remove_item(instance: XRGptItemInstance) -> bool:
+	for slot in _slots:
+		if slot.item == instance:
+			slot.clear_item()
+			return true
+	return false
